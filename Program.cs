e36c843c008053ee8b351a7961fc461bcd71de89
Masecy <Masecy@MASECY>
2025-05-20 08:22:16 -0600

    Actualizado

--- Program.cs
+++ Program.cs
@@ -1,52 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProyectoCajeroporConsola 
+namespace ProyectoCajeroporConsola
 {
     internal class Program
     {
-        public enum Menu 
+        public enum Menu
         {
-            ConsultarSalido=1,DepositarDinero,RetirarDinero,ConsultarHistorialdeDeposito,ConsultarHistorialdeRetiros 
+            ConsultarSaldo = 1, Depositar, Retirar, HistorialDepositos, HistorialRetiros, Salir
         }
-        static Dictionary<int,string>CajeroConsola = new Dictionary<int,string>();
+        double saldo = 0;
+        static Dictionary<DateTime, double> Depositos = new Dictionary<DateTime, double>();
+        static Dictionary<DateTime, double> Retiros = new Dictionary<DateTime, double>();
+
         static void Main(string[] args)
         {
-            DateTime fechaactual = DateTime.Now;
-                Console.WriteLine("Ingresa el usuario:");
-                string usuario = Console.ReadLine();
-                Console.WriteLine("Ingresa el pasword:");
-                string pasword = Console.ReadLine();
-                Console.WriteLine("Ingresa tu fecha de nacimiento:");
-                DateTime fechadenacimiento = Convert.ToDateTime(Console.ReadLine());
-                int edad = fechaactual.Year - fechadenacimiento.Year;
             int intentos = 3;
-            while (intentos <= 3)
+            do
             {
-                if (usuario == "diego" && pasword == "1234" && edad >= 18) 
+                if (login())
                 {
-                    Console.WriteLine("Ingreso exitoso");
+                    Console.WriteLine("Bienvenido a tu banco.");
+                    while (true)
+                    {
+                        switch (Men())
+                        {
+                            case Menu.ConsultarSaldo:
+                                Console.WriteLine($"Tu saldo es de: {saldo}.");
+                                break;
+                            case Menu.Depositar:
+                                Console.WriteLine("Introduce la cantidad a depositar: ");
+                                double deposito = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine($"Has depositado {deposito}");
+                                break;
+                            case Menu.Retirar:
+                                Console.WriteLine("Introduce la cantidad a retirar: ");
+                                double retiro = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine($"Has retirado {retiro}");
+                                break;
+                            case Menu.HistorialDepositos:
+
+                                break;
+                            case Menu.HistorialRetiros:
+
+                                break;
+                            case Menu.Salir:
+                                Console.WriteLine("Gracias por usar nuestro servicio.");
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Incorrecto");
+                    intentos--;
+                    Console.WriteLine($"Fallaste te quedan {intentos}");
+                }
+                if (intentos == 0)
+                {
+                    Console.WriteLine("Has fallado demasiadas veces, el programa se cerrará.");
+                    Console.ReadKey();
+                    Environment.Exit(1);
                 }
             }
-            
+            while (intentos >= 0);
         }
-        static  Menu Men() 
+
+        static bool login()
         {
-            
-            Console.WriteLine("1)Consultar saldo actual");
-            Console.WriteLine("2)Depositar dinero");
-            Console.WriteLine("3)Retirar dinero");
-            Console.WriteLine("4)Consultar historial de deposito");
-            Console.WriteLine("5)Consultar historial de retiros");
-            Console.WriteLine("6)Salir");
-            Menu opc =(Menu)Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce tu usuario: ");
+            string usuario = Console.ReadLine();
+            Console.WriteLine("Introduce tu contraseña: ");
+            string contrasena = Console.ReadLine();
+            DateTime fechaNacimiento = Convert.ToDateTime(Console.ReadLine);
+            DateTime fechaActual = DateTime.Now;
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+            if (usuario == "diego" && contrasena == "1234" && edad >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        static Menu Men()
+        {
+            Console.WriteLine("1) Consultar saldo actual");
+            Console.WriteLine("2) Depositar dinero");
+            Console.WriteLine("3) Retirar dinero");
+            Console.WriteLine("4) Consultar historial de depósito");
+            Console.WriteLine("5) Consultar historial de retiros");
+            Console.WriteLine("6) Salir");
+            Console.Write("Selecciona una opción: ");
+            Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
             return opc;
         }
     }
